b1839bd7739d5cfa2d5fb889619643ff10ae48b2
Levani Birchadze <levanbirchadze@gmail.com>
2023-06-21 19:27:17 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const initialBookState = {
+    title: '',
+    available: false,
+  }
+  const [book, setBook] = useState(initialBookState)
+
+  const updateBook = (book) => {
+    setBook({ title: book.title, available: book.available })
+  }
+
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
