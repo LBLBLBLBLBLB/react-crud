b05732b9aa1157643a53a89e2a0bc3e6a5694b53
Levani Birchadze <levanbirchadze@gmail.com>
2023-06-22 11:23:10 +0400

    feat: add user update function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -11,7 +12,10 @@ const App = () => {
   ]
 
   const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
 
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
 
   const addUser = (user) => {
     user.id = users.length + 1
@@ -22,17 +26,41 @@ const App = () => {
     setUsers(users.filter((user) => user.id !== id))
   }
 
+  const editRow = (user) => {
+    setEditing(true)
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)))
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser} />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
